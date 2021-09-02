2b357a73f8f51d15fbaaa35ba012bdae0c171a70
sanshenggui <1950648905@qq.com>
2021-09-02 11:20:42 +0800

    first commit

---
+++ preload.js
@@ -0,0 +1,12 @@
+
+// All of the Node.js APIs are available in the preload process.
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
